2e61e7a67b6313cb1cbe7f63fccf927e058db723
realsoftdev <final.fantasy.dev@gmail.com>
2017-03-24 08:23:28 -0400

    read data from xlsx

--- analyze.py
+++ analyze.py
@@ -1,5 +1,25 @@
-import numpy
+# import numpy
+import xlrd
 
 
+def readData():
+    workbook = xlrd.open_workbook('./data/Trade History.xlsx')
+    worksheet = workbook.sheet_by_index(0)
+    rows = []
+    for i, row in enumerate(range(worksheet.nrows)):
+        if i <= 1:
+            continue
+        r = []
+        for j, col in enumerate(range(worksheet.ncols)):
+            r.append(worksheet.cell_value(i, j))
+        rows.append(r)
+
+    return rows
+
+    
+def main():
+    data = readData()
+    print data
 
 if __name__ == "__main__":
+    main()
